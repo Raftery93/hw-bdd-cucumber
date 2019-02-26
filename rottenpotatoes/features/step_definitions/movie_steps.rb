0095ff76e80fe8098d6b08e36153ca7d49364291
Raftery93 <ec2-user@ip-172-31-27-80.us-east-2.compute.internal>
2019-02-26 16:43:22 +0000

    created test methods for ratings

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -27,13 +27,29 @@ end
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+################################
+When /^I check the following ratings: (.*)/ do |rating_list|
+  rating_list.split(", ").each do |rating|
+      step %Q{I check "ratings_#{rating}"}
+  end
 end
+When /^I uncheck the following ratings: (.*)/ do |rating_list|
+  rating_list.split(", ").each do |rating|
+      step %Q{I uncheck "ratings_#{rating}"}
+  end
+end
+Then /^I should see the following ratings: (.*)/ do |rating_list|
+  rating_list.split(", ").each do |text|  
+     # byebug
+     step %Q{I should see "#{text}"}
+  end
+end
+Then /^I should not see the following ratings: (.*)/ do |rating_list|
+  rating_list.split(", ").each do |text|
+    (page.body=~/\Atext\z/) == nil 
+  end
+end
+####################################
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
